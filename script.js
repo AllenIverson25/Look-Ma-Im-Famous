92e8f1445903dc6562daa3f7ecbcafd2a23b22ec
AllenIverson25 <526jfriedman@frhsd.com>
2024-10-22 11:34:24 -0400

    done kind of this time

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.et-hero-tabs-container');
-    if (window.scrollY > 100) { // Add a scroll threshold
+    if (window.scrollY > 80) { // Adjusted scroll threshold
         navbar.classList.add('et-hero-tabs-container--top');
     } else {
         navbar.classList.remove('et-hero-tabs-container--top');
